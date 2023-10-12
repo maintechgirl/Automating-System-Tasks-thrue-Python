96402fb8661fac183017246137edfe68db34cbad
Tatiana <127103022+maintechgirl@users.noreply.github.com>
2023-10-13 00:56:19 +0200

    Add files via upload

--- Managing_Data_and_Processes.py
+++ Managing_Data_and_Processes.py
@@ -1,5 +1,5 @@
-#Python Subprocesses
 
+###Python Subprocesses
 
 
 
@@ -55,4 +55,127 @@ my_env["PATH"] = os.pathsep.join(["/opt/myapp/", my_env["PATH"]])
 
 result = subprocess.run(["myapp"], env=my_env)
 
+
+
+###Processing Lof Files
+
+
+#Filtering Log Files with Regular Expressions
+
+#check_cron.py     
+#!/usr/bin/env python3
+import sys
+
+logfile = sys.argv[1]
+with open (logfile) as f:
+    for line in f:
+        if "CRON" not in line:
+            continue
+        print(line.strip())
+
+
+#cat syslog
+#Jul 6 14:04:07 computer.name CRON[29440]: USER (good_user)
+#Jul 6 14:04:07 computer.name CRON[29187]: USER (UUID:007)
+#Jul 6 14:04:07 computer.name CRON[29440]: USER (naughty_user)
+#Jul 6 14:04:07 computer.name CRON[29187]: USER (UUID:008)
+#Jul 6 14:04:07 computer.name CRON[29440]: USER (naughty_user)
+
+import re
+
+pattern = r"USER \((\w+)\)$"
+line = "Jul 6 14:04:07 computer.name CRON[29440]: USER (naughty_user)"
+result = re.search(pattern, line)
+print(result[1])
+#naughty_user
+
+
+
+import re
+import sys
+
+logfile = sys.argv[1]
+with open (logfile) as f:
+    for line in f:
+        if "CRON" not in line:
+            continue
+        pattern = r"USER \((\w+)\)$"
+        result = re.search(pattern, line)
+
+        print(result[1])
+
+#cmod +x check_cron.py 
+#./check_cron.py syslog
+# 
+# good_user
+# naughty_user
+# naughty_user       
+
+
+
+
+
+#Problem: We're using the same syslog, and we want to display the date, time, 
+# and process id that's inside the square brackets. We can read each line of the syslog and pass the contents 
+# to the show_time_of_pid function. Fill in the gaps to extract the date, time, and process id from the passed line, 
+# and return this format: Jul 6 14:01:23 pid:29440.
+
+import re
+def show_time_of_pid(line):
+  pattern = r'^(\w+ [0-9] [0-9]+:[0-9]+:[0-9]+) [\w\.]+ [\w=]+\[([0-9]+)\]'
+  result = re.search(pattern, line)
+  return '{} pid:{}'.format(result[1],result[2])
+
+print(show_time_of_pid("Jul 6 14:01:23 computer.name CRON[29440]: USER (good_user)")) # Jul 6 14:01:23 pid:29440
+
+print(show_time_of_pid("Jul 6 14:02:08 computer.name jam_tag=psim[29187]: (UUID:006)")) # Jul 6 14:02:08 pid:29187
+
+print(show_time_of_pid("Jul 6 14:02:09 computer.name jam_tag=psim[29187]: (UUID:007)")) # Jul 6 14:02:09 pid:29187
+
+print(show_time_of_pid("Jul 6 14:03:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:03:01 pid:29440
+
+print(show_time_of_pid("Jul 6 14:03:40 computer.name cacheclient[29807]: start syncing from \"0xDEADBEEF\"")) # Jul 6 14:03:40 pid:29807
+
+print(show_time_of_pid("Jul 6 14:04:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:04:01 pid:29440
+
+print(show_time_of_pid("Jul 6 14:05:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:05:01 pid:29440
+
+
+
+
+
+
+#Making Sense out of the Data
+
+usernames = {}
+name = "good_user"
+usernames[name] = usernames.get(name, 0) +1
+print(usernames)
+#{'good_user': 1}
+usernames[name] = usernames.get(name, 0) +1
+print(usernames)
+#{'good_user': 2}
+
+
+import re
+import sys
+
+logfile = sys.argv[1]
+usernames = {}
+with open (logfile) as f:
+    for line in f:
+        if "CRON" not in line:
+            continue
+        pattern = r"USER \((\w+)\)$"
+        result = re.search(pattern, line)
+        if result is None:
+            continue
+        name = result[1]
+        usernames[name] = usernames.get(name, 0) +1
+print(usernames)
+
+
+#./check_cron.py syslog
+#
+#{'good_user': 1, 'naughty_user': 2}
 
