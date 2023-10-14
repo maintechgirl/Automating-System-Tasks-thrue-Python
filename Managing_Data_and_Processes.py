8842b4182e8285d15da2888d6ec363559f0d385e
Tatiana <127103022+maintechgirl@users.noreply.github.com>
2023-10-14 17:37:56 +0200

    search_error

--- Managing_Data_and_Processes.py
+++ Managing_Data_and_Processes.py
@@ -1,5 +1,5 @@
+#Python Subprocesses
 
-###Python Subprocesses
 
 
 
@@ -127,19 +127,12 @@ def show_time_of_pid(line):
   return '{} pid:{}'.format(result[1],result[2])
 
 print(show_time_of_pid("Jul 6 14:01:23 computer.name CRON[29440]: USER (good_user)")) # Jul 6 14:01:23 pid:29440
-
 print(show_time_of_pid("Jul 6 14:02:08 computer.name jam_tag=psim[29187]: (UUID:006)")) # Jul 6 14:02:08 pid:29187
-
 print(show_time_of_pid("Jul 6 14:02:09 computer.name jam_tag=psim[29187]: (UUID:007)")) # Jul 6 14:02:09 pid:29187
-
 print(show_time_of_pid("Jul 6 14:03:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:03:01 pid:29440
-
 print(show_time_of_pid("Jul 6 14:03:40 computer.name cacheclient[29807]: start syncing from \"0xDEADBEEF\"")) # Jul 6 14:03:40 pid:29807
-
 print(show_time_of_pid("Jul 6 14:04:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:04:01 pid:29440
-
 print(show_time_of_pid("Jul 6 14:05:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:05:01 pid:29440
-
 
 
 
@@ -179,3 +172,46 @@ print(usernames)
 #
 #{'good_user': 1, 'naughty_user': 2}
 
+
+
+###Working with Log Files 
+
+#Introduction
+
+#Imagine one of your colleagues is struggling with a program that keeps throwing an error. Unfortunately, the program's source code is too complicated to easily find the error there. The good news is that the program outputs a log file you can read! Let's write a script to search the log file for the exact error, then output that error into a separate file so you can work out what's wrong.
+#What you'll do
+
+#    Write a script to search the log file using regex to find for the exact error.
+#    Report the error into a separate file so you know what's wrong for further analysis.
+
+
+#!/usr/bin/env python3
+import sys
+import os
+import re
+
+
+def error_search(log_file):
+  error = input("What is the error? ")
+  returned_errors = []
+  with open(log_file, mode='r',encoding='UTF-8') as file:
+    for log in  file.readlines():
+      error_patterns = ["error"]
+      for i in range(len(error.split(' '))):
+        error_patterns.append(r"{}".format(error.split(' ')[i].lower()))
+      if all(re.search(error_pattern, log.lower()) for error_pattern in error_patterns):
+        returned_errors.append(log)
+    file.close()
+  return returned_errors
+
+  
+def file_output(returned_errors):
+  with open(os.path.expanduser('~') + '/data/errors_found.log', 'w') as file:
+    for error in returned_errors:
+      file.write(error)
+    file.close()
+if __name__ == "__main__":
+  log_file = sys.argv[1]
+  returned_errors = error_search(log_file)
+  file_output(returned_errors)
+  sys.exit(0)
