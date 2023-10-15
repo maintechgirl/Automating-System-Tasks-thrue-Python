c88d3eb21f215f0de60a74fd1d97d6c3b1987b51
Tatiana <127103022+maintechgirl@users.noreply.github.com>
2023-10-15 22:14:54 +0200

    The Try-Except Construct; Raising Errors

--- Testing.py
+++ Testing.py
@@ -169,17 +169,17 @@ unittest.main(argv = ['first-arg-is-ignored'], exit = False)
 
 # EDGE CASES HERE
 
-    def test_empty(self):
+def test_empty(self):
         testcase = ""
         expected = ""
         self.assertEqual(LetterCompiler(testcase), expected)
         
-    def test_word(self):
+def test_word(self):
         testcase = "awesome"
         expected = ['a']
         self.assertEqual(LetterCompiler(testcase), expected)
         
-    def test_unmatch(self):
+def test_unmatch(self):
         testcase = "go"
         expected = []
         self.assertEqual(LetterCompiler(testcase), expected)
@@ -189,12 +189,119 @@ if __name__ == "__main__":
 
 
 
+### The Try-Except Construct
+
+
+def read_file(filename):
+    if not os.path.exist(filename):
+        return ""
+    if not os.path.isfile(filename):
+        return ""
+    if not os.access(filename, os.R_OK):
+        return ""
+    if is_locked(filename):
+        return ""
+    if if_not_accessible(filename):
+        return ""
 
 
 
 
 
+# The Try-Except Construct Exzample
+
+def character_frequency(filename):
+    """Count a frequency of the each character in the given file"""
+    #First try to open the file
+    try: 
+        f = open(filename)
+    except OSError:
+        return None
+    
+    #Now process the file
+    characters = {}
+    for line in f:
+        for char in line:
+            characters[char] = characters.get(char, 0) + 1
+    f.close()
+    return characters
 
 
 
 
+#Raising Errors
+
+def validate_user(username, minlen):
+    if minlen < 1:
+        raise ValueError("minlen must be at least 1")
+    if len(username) < minlen:
+        return False
+    if not username.isalnum():
+        return False
+    return True
+
+#from validations import validate_user
+#validate_user("", -1)
+#
+#ValueError: minlen must be at least 1
+
+#validate_user("", 1)
+#
+#False
+
+#validate_user("myuser", 1)
+#
+#True
+
+#validate_user(88, 1)
+#
+#TyperError: object of type int has no len()
+
+#validate_user([], 1)
+#
+#False
+
+#validate_user(["name"], 1)
+#
+#AttributeError: list object has no attribute isalnum
+
+def validate_user(username, minlen):
+    asser type(username) == str, "username must be a string"
+    if minlen < 1:
+        raise ValueError("minlen must be at least 1")
+    if len(username) < minlen:
+        return False
+    if not username.isalnum():
+        return False
+    return True
+
+#from validations import validate_user
+
+#validate_user([2], 1)
+#
+#AssertionErrorr: username must be a string
+
+
+
+
+
+#Testing for Expected Errors
+
+import unittest
+
+from validations import validate_user
+
+class TestValidateUser(unittest.TestCase):
+    def test_valid(self):
+        self.assertEqual(validate_user("validuser", 3), True)
+    
+    def too_short(self):
+        self.assertEqual(validate_user("inv", 5), False)
+
+    def test_invalid_characters(self):
+        self.assertEqual(validate_user("invalid user", 1), False)
+
+    def test_invalid_minlen(self):
+        self.assertRaises(ValueError, validate_user, "user", -1)
+
+unittest.main()
