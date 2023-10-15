f5e6e262b2766e4430ec640a1d4d876ddb9153b8
Tatiana <127103022+maintechgirl@users.noreply.github.com>
2023-10-15 23:00:15 +0200

    Practice Errors and Exceptions

--- Testing.py
+++ Testing.py
@@ -305,3 +305,82 @@ class TestValidateUser(unittest.TestCase):
         self.assertRaises(ValueError, validate_user, "user", -1)
 
 unittest.main()
+
+
+#Practice Errors and Exceptions
+
+my_list = [27, 5, 9, 6, 8]
+
+def RemoveValue(myVal):
+    my_list.remove(myVal)
+    return my_list
+
+print(RemoveValue(27))  # [5, 9, 6, 8]
+
+print(RemoveValue(27)) #ValueError: list.remove(x): x not in list
+
+def RemoveValue(myVal):
+    if myVal not in my_list:
+        raise ValueError("Value must be in the given list")
+    else:
+        my_list.remove(myVal)
+    return my_list
+
+print(RemoveValue(27)) #ValueError: Value must be in the given list
+
+
+
+my_word_list = ['east', 'after', 'up', 'over', 'inside']
+
+def OrganizeList(myList):
+    myList.sort()
+    return myList
+
+print(OrganizeList(my_word_list)) # ['after', 'east', 'inside', 'over', 'up']
+
+my_new_list = [6, 3, 8, "12", 42]
+print(OrganizeList(my_new_list)) #TypeError: '<' not supported between instances of 'str' and 'int'
+
+def OrganizeList(myList):
+    for item in myList:
+        assert type(myList)==str, "Word list must be a list of strings"
+    myList.sort()
+    return myList
+
+print(OrganizeList(my_new_list)) # AssertionError: Word list must be a list of strings
+
+
+
+import random
+
+participants = ['Jack','Jill','Larry','Tom']
+
+def Guess(participants):
+    my_participant_dict = {}
+    for participant in participants:
+        my_participant_dict[participant] = random.randint(1, 9)
+    if my_participant_dict['Larry'] == 9:
+        return True
+    else:
+        return False
+    
+print(Guess(participants)) #False
+
+# Revised Guess() function
+def Guess(participants):
+    my_participant_dict = {}
+    for participant in participants:
+        my_participant_dict[participant] = random.randint(1, 9)
+        try:
+            f = open(participants)
+            if my_participant_dict['Larry'] == 9:
+                return True
+            else:
+                return False
+        except TypeError:
+                return None
+    
+participants = ['Cathy','Fred','Jack','Tom']
+print(Guess(participants)) #None
+
+
